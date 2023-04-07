bbe126a2e34b6e323ede04e329d5b8345adb219b
limjonggak <limjonggak@naver.com>
2023-04-07 17:13:32 +0900

    업데이트

--- src/main/java/twogtwoj/whereishere/web/BasicController.java
+++ src/main/java/twogtwoj/whereishere/web/BasicController.java
@@ -2,13 +2,11 @@ package twogtwoj.whereishere.web;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import twogtwoj.whereishere.domain.Category;
 import twogtwoj.whereishere.domain.Company;
 import twogtwoj.whereishere.domain.Member;
-import twogtwoj.whereishere.repository.StarRepository;
 import twogtwoj.whereishere.service.CompanyService;
 import twogtwoj.whereishere.service.MemberService;
 
@@ -23,6 +21,10 @@ public class BasicController {
     private final MemberService memberService;
 
     private final CompanyService companyService;
+
+
+    @GetMapping("/home")
+    public String enterHome() {return "/home/home";}
 
     @PostConstruct
     public void initMemberAndCompany() {
